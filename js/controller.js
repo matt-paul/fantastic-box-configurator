672fe3f5dd904d003f62bde6a75f783387f07e7d
Matt Paul <mattpaul001@icloud.com>
2015-12-03 14:08:19 +0000

    Step 2 unit tests complete

--- js/controller.js
+++ js/controller.js
@@ -5,4 +5,19 @@ app.controller('Controller', ['calculateFactory', function(calculateFactory) {
   this.calculateArea = function(width, length, height) {
     return width * length * height;
   };
+
+  this.cardboardGradeCalc = function(grade, area) {
+    switch (grade) {
+      case 'A':
+        return 0.20 * area;
+      case 'B':
+        return 0.10 * area;
+      case 'C':
+        if (area > 2) throw new Error("Grade C box must be below 2m squared");
+        return 0.05 * area;
+    }
+    
+    // return grade * area;
+  };
+
 }]);
--- test/unit/controller.spec.js
+++ test/unit/controller.spec.js
@@ -2,6 +2,7 @@ describe('Controller', function() {
   beforeEach(module('FantasticBoxOrder'));
 
   var ctrl;
+  // var a;
 
   beforeEach(inject(function($controller) {
     ctrl = $controller('Controller');
@@ -9,5 +10,25 @@ describe('Controller', function() {
 
   it('calculates the area of a box', function() {
     expect(ctrl.calculateArea(2,2,2)).toEqual(8);
+  });
+
+  describe('cardboard grade calculation', function() {
+
+    it('correctly calculates the price of a grade A box', function() {
+      expect(ctrl.cardboardGradeCalc('A', 2)).toEqual(0.40);
+    });
+
+    it('correctly calculates the cost of a grade B box', function() {
+      expect(ctrl.cardboardGradeCalc('B', 2)).toEqual(0.20);
+    });
+
+    it('correctly calculares the cost of a grade C box', function() {
+      expect(ctrl.cardboardGradeCalc('C', 2)).toEqual(0.10);
+    });
+
+    it('errors if grade C box larger than 2m is requested', function() {
+      expect( function(){ ctrl.cardboardGradeCalc('C', 2.1); } ).toThrow(new Error("Grade C box must be below 2m squared"));
+    });
+
   });
 });
